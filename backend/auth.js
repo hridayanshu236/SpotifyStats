bc9ba4ce3cbe67aae98e21f3dae5693929953210
Hridayanshu Raj Acharya <148516090+hridayanshu236@users.noreply.github.com>
2024-09-01 17:28:13 +0545

    backend .env setup

--- backend/auth.js
+++ backend/auth.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 
 const router = express.Router();
 
-const redirect_uri = `${process.env.FRONTEND_URL}/auth/callback`;
+const redirect_uri = `${process.env.BACKEND_URL}/callback`;
 router.use(cookieParser());
 
 router.get('/login', (req, res) => {
